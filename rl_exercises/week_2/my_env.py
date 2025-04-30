d416a4377a9bc52a87f146afa6a40717a08b5141
trolololoojb <j-u-b+github@outlook.de>
2025-04-30 18:53:24 +0200

    level 1

--- rl_exercises/week_2/my_env.py
+++ rl_exercises/week_2/my_env.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 import gymnasium as gym
+import numpy as np
+from gymnasium import spaces
 
 
 # ------------- TODO: Implement the following environment -------------
@@ -31,7 +33,43 @@ class MyEnv(gym.Env):
 
     def __init__(self):
         """Initializes the observation and action space for the environment."""
-        pass
+        self.observation_space = spaces.Discrete(2)
+        self.action_space = spaces.Discrete(2)
+        self.state = 0  # always start in state 0
+
+    def reset(self, *, seed=None, options=None):
+        """Reset environment to initial state."""
+        self.state = 0
+        return int(self.state), {}
+
+    def step(self, action):
+        """Perform one step in the environment."""
+        if not self.action_space.contains(action):
+            raise RuntimeError(f"Invalid action: {action}")
+
+        self.state = action  # action is either 0 or 1-> go to that state
+        reward = float(action)  # reward is equal to the action
+        terminated = False
+        truncated = False
+        info = {}
+
+        return int(self.state), reward, terminated, truncated, info
+
+    def get_reward_per_action(self):
+        """Returns the reward for each (state, action) pair as a matrix."""
+        R = np.zeros((2, 2), dtype=float)
+        for s in range(2):
+            for a in range(2):
+                R[s, a] = float(a)  # reward is equal to te action
+        return R
+
+    def get_transition_matrix(self):
+        """Returns the transition probability matrix T[s, a, s']"""
+        T = np.zeros((2, 2, 2), dtype=float)
+        for s in range(2):
+            T[s, 0, 0] = 1.0  # action 0 -> always go to state 0
+            T[s, 1, 1] = 1.0  # action 1 -> always go to state 1
+        return T
 
 
 class PartialObsWrapper(gym.Wrapper):
